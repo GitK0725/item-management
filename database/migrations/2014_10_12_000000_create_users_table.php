fd0e92d8298bb5b7c6a10fe664e8a8fcc791042f
GitK0725 <70615341+GitK0725@users.noreply.github.com>
2023-03-17 22:15:27 +0900

    add increments(id)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('email', 255)->charset("utf8")->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->tinyInteger('role')->index();
