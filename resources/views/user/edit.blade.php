bc5a8044e08d1f076c0f43f93b4d84f20f927efe
GitK0725 <70615341+GitK0725@users.noreply.github.com>
2023-03-18 17:51:48 +0900

    add delete edit.blade

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -27,5 +27,6 @@
             <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
         </div>
         <button type="submit" class="btn btn-primary">Save</button>
+  <a class="btn btn-secondary" href="/user/delete/{{ $user->id }}">削除</a>
     </form>
 @stop
