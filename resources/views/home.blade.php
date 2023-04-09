a7342a4be38a58621edbe6c169bacc16fb0c1300
GitK0725 <70615341+GitK0725@users.noreply.github.com>
2023-04-09 12:21:36 +0900

    表記文字変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,11 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>サッカー用具専門店</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <p>こんにちは！</p>
 @stop
 
 @section('css')
--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -11,21 +11,21 @@
         @csrf
         <input type="hidden" name="id" value="{{ $user->id }}">
         <div class="form-group">
-            <label for="name">Name</label>
+            <label for="name">名前（フルネーム）</label>
             <input type="text" name="name" id="name" class="form-control" value="{{ $user->name }}">
         </div>
         <div class="form-group">
-            <label for="email">Email</label>
+            <label for="email">メールアドレス</label>
             <input type="email" name="email" id="email" class="form-control" value="{{ $user->email }}">
         </div>
         <div class="form-group">
-            <label for="password">Password</label>
+            <label for="password">パスワード</label>
             <input type="password" name="password" id="password" class="form-control">
         </div>
         <div class="form-group">
-            <label for="password_confirmation">Confirm Password</label>
+            <label for="password_confirmation">パスワード（確認）</label>
             <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
-        <label for="role">role</label>
+        <label for="role">権限</label>
         <td><input type="radio" name="role" value="1" @if ($user->role == 1) checked @endif required>管理者</td></td>
         <td><input type="radio" name="role" value="0" @if ($user->role == 0) checked @endif required>利用者</td></td> 
         </div>
