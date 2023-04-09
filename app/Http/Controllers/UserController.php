47b87536811ce2d94a409b453ba46a50162aa6d1
GitK0725 <70615341+GitK0725@users.noreply.github.com>
2023-04-09 19:04:28 +0900

    バリデーション　ユーザー編集　edit.blade,UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,6 +28,17 @@ class UserController extends Controller
     }
     public function update(Request $request)
     {
+        $validated = $request->validate([
+            'name' => 'required|unique:items|max:255',
+            'email' => 'required',
+            'password' => 'confirmed',
+        ],[
+                'name.required' => '名前を入力してください。',
+                'email.required' => 'メールアドレスを入力してください。',
+                'password.confirmed' => 'パスワードが一致しません。',
+            ]);
+        
+
         $user=User::find($request->id)->update([
             'name' => $request->name,
             'role' => $request->role,
